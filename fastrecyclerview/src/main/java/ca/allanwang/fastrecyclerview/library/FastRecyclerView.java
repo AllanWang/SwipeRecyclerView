656b67832e77bf5c09b24c27ec4e4dc25e642231
AllanWang <me@allanwang.ca>
2017-02-06 17:06:17 -0500

    classes

---
+++ fastrecyclerview/src/main/java/ca/allanwang/fastrecyclerview/library/FastRecyclerView.java
@@ -0,0 +1,8 @@
+package ca.allanwang.fastrecyclerview.library;
+
+/**
+ * Created by Allan Wang on 2017-02-06.
+ */
+
+public class FastRecyclerView {
+}
---
+++ sample/src/main/java/ca/allanwang/fastrecyclerview/sample/MainActivity.java
@@ -0,0 +1,8 @@
+package ca.allanwang.fastrecyclerview.sample;
+
+/**
+ * Created by Allan Wang on 2017-02-06.
+ */
+
+public class MainActivity {
+}
