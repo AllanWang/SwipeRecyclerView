48d001d75c8e123f397153396de0a1450a4edd90
AllanWang <me@allanwang.ca>
2017-02-06 17:41:12 -0500

    setup base

--- sample/src/main/java/ca/allanwang/fastrecyclerview/sample/MainActivity.java
+++ sample/src/main/java/ca/allanwang/fastrecyclerview/sample/MainActivity.java
@@ -1,8 +1,25 @@
 package ca.allanwang.fastrecyclerview.sample;
 
+import android.support.annotation.Nullable;
+
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+
+import ca.allanwang.capsule.library.activities.CapsuleActivityFrame;
+import ca.allanwang.capsule.library.interfaces.CDrawerItem;
+
 /**
  * Created by Allan Wang on 2017-02-06.
  */
 
-public class MainActivity {
+public class MainActivity extends CapsuleActivityFrame {
+    @Nullable
+    @Override
+    protected AccountHeaderBuilder getAccountHeaderBuilder() {
+        return null;
+    }
+
+    @Override
+    protected CDrawerItem[] getDrawerItems() {
+        return new CDrawerItem[0];
+    }
 }
